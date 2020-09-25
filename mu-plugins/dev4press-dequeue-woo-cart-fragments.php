821d87428c523285a5f6d07a932e1b7349510b7f
Milan Petrovic <milan@gdragon.info>
2020-09-25 17:59:14 +0200

    One more plugin added.

---
+++ mu-plugins/dev4press-dequeue-woo-cart-fragments.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Plugin Name:       Dev4Press WPMU Snippets: Dequeue WooCommerce Cart Fragments
+Plugin URI:        https://github.com/dev4press/wpmu-plugins
+Description:       Embed jQuery inside of the page HEAD for better cache performance. Works with the WordPress 4.9 or newer.
+Author:            Milan Petrovic - Dev4Press
+Author URI:        https://www.dev4press.com/
+Version:           1.0
+Requires at least: 4.9
+Tested up to:      5.5
+Requires PHP:      5.6
+License:           GNU GeneralPublic License v3 or later
+License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+add_action( 'wp_enqueue_scripts', 'dev4press__dequeue_woo_cart_fragments', 20 );
+
+function dev4press__dequeue_woo_cart_fragments() {
+	if ( ! is_post_type_archive('product') && ! is_singular('product') ) {
+		wp_dequeue_script( 'wc-cart-fragments' );
+	}
+}
