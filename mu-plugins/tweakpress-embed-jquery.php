66371625c179753cf4891204d70fd40091a2fbaf
Milan Petrovic <dev4press@users.noreply.github.com>
2021-12-29 09:28:29 +0100

    Update tweakpress-embed-jquery.php
    Now embedding minimized version of jQuery.

--- mu-plugins/tweakpress-embed-jquery.php
+++ mu-plugins/tweakpress-embed-jquery.php
@@ -6,9 +6,9 @@ Plugin URI:        https://github.com/dev4press/tweakpress
 Description:       Embed jQuery inside of the page HEAD for better cache performance. Works with the WordPress 4.9 or newer.
 Author:            Milan Petrovic - Dev4Press
 Author URI:        https://www.dev4press.com/
-Version:           1.0
+Version:           1.1
 Requires at least: 4.9
-Tested up to:      5.5
+Tested up to:      5.9
 Requires PHP:      5.6
 License:           GNU GeneralPublic License v3 or later
 License URI:       http://www.gnu.org/licenses/gpl-3.0.html
@@ -17,7 +17,7 @@ License URI:       http://www.gnu.org/licenses/gpl-3.0.html
 if ( ! is_admin() ) {
 	add_action( 'wp_head', 'tweakpress__embed_jquery', 0 );
 	function tweakpress__embed_jquery() {
-		$path = ABSPATH . 'wp-includes/js/jquery/jquery.js';
+		$path = ABSPATH . 'wp-includes/js/jquery/jquery.min.js';
 		$file = file_get_contents( $path );
 
 		?>
