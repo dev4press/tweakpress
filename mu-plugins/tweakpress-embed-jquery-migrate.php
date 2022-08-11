c858d8912d6406da1a51be367f7f554d87ca55ea
Milan Petrovic <milan@gdragon.info>
2022-08-11 11:18:14 +0200

    Updated few tweaks.

--- mu-plugins/tweakpress-embed-jquery-migrate.php
+++ mu-plugins/tweakpress-embed-jquery-migrate.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
-Plugin Name:       TweakPress: Embed jQuery
+Plugin Name:       TweakPress: Embed jQuery Migrate
 Plugin URI:        https://github.com/dev4press/tweakpress
-Description:       Embed jQuery inside the page HEAD for better cache performance. Works with the WordPress 4.9 or newer.
+Description:       Embed jQuery Migrate inside the page HEAD for better cache performance. Works with the WordPress 4.9 or newer.
 Author:            Milan Petrovic - Dev4Press
 Author URI:        https://www.dev4press.com/
-Version:           1.3
+Version:           1.0
 Requires at least: 4.9
 Tested up to:      6.0
 Requires PHP:      5.6
@@ -15,30 +15,30 @@ License URI:       http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 if ( ! is_admin() ) {
-	add_action( 'wp_head', 'tweakpress__embed_jquery', 0 );
-	add_action( 'login_head', 'tweakpress__embed_jquery', 0 );
-	function tweakpress__embed_jquery() {
-		$path = ABSPATH . 'wp-includes/js/jquery/jquery.min.js';
+	add_action( 'wp_head', 'tweakpress__embed_jquery_migrate', 1 );
+	add_action( 'login_head', 'tweakpress__embed_jquery_migrate', 1 );
+	function tweakpress__embed_jquery_migrate() {
+		$path = ABSPATH . 'wp-includes/js/jquery/jquery-migrate.min.js';
 		$file = file_get_contents( $path );
 
 		?>
 
-        <script id="embedded-jquery-core" type="text/javascript"><?php echo $file; ?></script>
+        <script id="embedded-jquery-migrate" type="text/javascript"><?php echo $file; ?></script>
 
 		<?php
 	}
 
-	add_action( 'wp_default_scripts', 'tweakpress__replace_jquery', 10 );
-	function tweakpress__replace_jquery( $scripts ) {
-		$script = $scripts->query( 'jquery-core', 'registered' );
+	add_action( 'wp_default_scripts', 'tweakpress__replace_jquery_migrate', 10 );
+	function tweakpress__replace_jquery_migrate( $scripts ) {
+		$script = $scripts->query( 'jquery-migrate', 'registered' );
 
 		if ( $script ) {
-			tweakpress__set_script( $scripts, 'jquery-core', '', array(), $script->ver );
+			tweakpress__set_script( $scripts, 'jquery-migrate', '', array(), $script->ver );
 		}
 	}
 }
 
-if (!function_exists('tweakpress__set_script')) {
+if ( ! function_exists( 'tweakpress__set_script' ) ) {
 	function tweakpress__set_script( $scripts, $handle, $src, $deps = array(), $ver = false, $in_footer = false ) {
 		$script = $scripts->query( $handle, 'registered' );
 
